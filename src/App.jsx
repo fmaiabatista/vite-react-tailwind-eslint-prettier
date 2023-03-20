04c2690cbda186c5282aaf1985a419d836d04f8f
Felipe Cruz <fcr@foryouandyourcustomers.com>
2023-03-20 20:05:04 +0100

    add eslint, prettier; tooling config

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1 className="text-blue-700 text-3xl font-bold">
+    <div className="mt-40 flex h-screen w-screen flex-col items-center">
+      <h1 className="bg-gradient-to-br from-teal-400 to-blue-600 bg-clip-text text-4xl font-extrabold text-transparent">
         Vite + React + Tailwind
       </h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="border border-gray-400 rounded-lg px-3 py-2 mb-3"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+      <button
+        onClick={() => setCount((count) => count + 1)}
+        className="my-3 rounded-lg border border-gray-400 px-3 py-2"
+      >
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
       </p>
     </div>
   );
-}
+};
 
 export default App;
